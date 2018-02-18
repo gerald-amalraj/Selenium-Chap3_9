cf719d7b33fa89f39ba29ef0fab5cabac0e836b0
gerald-amalraj <gerald.amalraj@gmail.com>
2018-02-18 12:02:25 +0530

    wroking file

--- src/test/java/packt/selenium/chap3_9/SearchGoogleTest.java
+++ src/test/java/packt/selenium/chap3_9/SearchGoogleTest.java
@@ -23,7 +23,7 @@ public class SearchGoogleTest {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.MINUTES);
     }
     @After
     public void tearDown() throws Exception {
